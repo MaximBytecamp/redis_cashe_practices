145e0fe80d89ea13c9ca8f8011633a84ccd0ecdc
MaximBytecamp <bytecampmm@gmail.com>
2026-03-24 20:42:46 +0300

    Add configuration settings for Redis connection and application parameters

---
+++ redis_ttl_jitter/app/config.py
@@ -0,0 +1,35 @@
+from __future__ import annotations 
+
+
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+#Path(/Users/makarovmn/Public/redis_cashe_practices/redis_ttl_jitter/.env)
+_ENV_FILE = Path(__file__).resolve().parent.parent / ".env"
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file = str(_ENV_FILE),
+        env_file_encoding="utf-8"
+    )
+
+    redis_host: str
+    redis_port: int
+    redis_db: int
+
+    base_ttl: int = 60          # секунд
+    jitter_max: int = 20        # максимальное отклонен
+
+
+    app_host: str = "0.0.0.0"
+    app_port: int = 8000
+
+    @property
+    def redis_url(self) -> str:
+        return f"redis://{self.redis_host}:{self.redis_port}/{self.redis_db}"
+    
+
+settings = Settings()
+    
